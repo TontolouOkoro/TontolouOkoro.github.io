cd0b7298c4af4e78244f182372f49c17bb3c0ac1
TontolouOkoro <dainefroste@gmail.com>
2026-09-10 09:19:34 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,52 @@
+import { useState} from 'react';
+import  './App.css'
+
+function App () {
+    const [input, setInput] = useState("");
+
+    const handleClick = (value) => {
+        setInput((prev) => prev + value);
+    };
+
+    const handleDel = () => {
+        setInput((prev) => prev.slice(0,-1));
+    }
+
+    const handleCalc = () => {
+        try {
+            setInput((eval(input)).toString());
+        } catch (e) {
+            setInput("error");
+        }
+        
+    }
+
+    return (
+            <div className="calculator">
+
+                <div className="display">
+                    {input || "0"}
+                </div>
+
+                <div className='buttons'>
+                    <button onClick={handleDel}>DEL</button>
+                    <button onClick={() => {setInput("")}}>AC</button>
+                    <button onClick={()=>{handleClick("+")}}>+</button>
+                    <button onClick={()=>{handleClick("-")}}>-</button>
+                    <button onClick={()=>{handleClick("1")}}>1</button>
+                    <button onClick={()=>{handleClick("2")}}>2</button>
+                    <button onClick={()=>{handleClick("3")}}>3</button>
+                    <button onClick={()=>{handleClick("4")}}>4</button>
+                    <button onClick={()=>{handleClick("5")}}>5</button>
+                    <button onClick={()=>{handleClick("6")}}>6</button>
+                    <button onClick={()=>{handleClick("7")}}>7</button>
+                    <button onClick={()=>{handleClick("8")}}>8</button>
+                    <button onClick={()=>{handleClick("9")}}>9</button>
+                    <button onClick={()=>{handleClick("0")}}>0</button>
+                    <button onClick={handleCalc}>=</button>
+                </div>
+            </div>
+    );
+}
+
+export default App;
